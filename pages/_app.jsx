b2b47ba623da97e077dbf2c3e93d33c24295d628
takascemberi1 <takascemberi1@gmail.com>
2025-10-02 22:48:07 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,17 @@
 // pages/_app.jsx
 import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
+  const pk = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={pk}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Üreten Eller</title>
+      </Head>
       <Component {...pageProps} />
     </ClerkProvider>
   );
