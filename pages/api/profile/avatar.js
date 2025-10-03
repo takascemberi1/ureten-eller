3d6f31749393101702eafacdf3d0278a762bfd5c
takascemberi1 <takascemberi1@gmail.com>
2025-10-03 18:17:20 +0300

    Create avatar.js

---
+++ pages/api/profile/avatar.js
@@ -0,0 +1,3 @@
+import { getAuth } from "@clerk/nextjs/server";
+export const config = { api: { bodyParser: { sizeLimit: '6mb' } } };
+export default async function handler(req, res) { /* avatarHandler içeriği */ }
