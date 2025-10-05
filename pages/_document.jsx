f567598ed37c1ba82db3b946948a2c7a055b5118
takascemberi1 <takascemberi1@gmail.com>
2025-10-05 23:59:59 +0000

    fix: add /favicon.ico and link in head

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,12 +3,9 @@ export default function Document() {
   return (
     <Html lang="tr">
       <Head>
-        <link rel="icon" href="/assets/images/logo.png" type="image/png" />
+        <link rel="icon" href="/favicon.ico?v=1" />
       </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+      <body><Main /><NextScript /></body>
     </Html>
   );
 }
