437b7692187563c295451950101db9e4aec1fe43
takascemberi1 <takascemberi1@gmail.com>
2025-10-05 23:57:36 +0000

    chore: add favicon link across pages + home.html (and app/head)

---
+++ app/head.jsx
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/assets/images/logo.png" type="image/png" />
+    </>
+  );
+}
---
+++ pages/_document.jsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export default function Document() {
+  return (
+    <Html lang="tr">
+      <Head>
+        <link rel="icon" href="/assets/images/logo.png" type="image/png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
